30c066fafa3b636dade4ba731a5ee39abf35641d
JoeyOBrien <s00131899@mail.itsligo.ie>
2014-12-08 23:09:54 +0000

    commit

---
+++ ca2/Controllers/HomeController.cs
@@ -0,0 +1,148 @@
+﻿using ca2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ca2.Controllers
+{
+    public class HomeController : Controller
+    {
+        MovieDb db = new MovieDb();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id)
+        {
+            var m = db.Movies.Find(id);
+            if (m != null)//if movie is found
+            {
+                ViewBag.SexStatsMale = m.Actors.Count(act => act.Sex == Sex.Male);
+                ViewBag.SexStatsFemale = m.Actors.Count(act => act.Sex == Sex.Female);
+                return View(m);
+            }
+            else//not found
+                return View();
+        }
+
+        public PartialViewResult ActorsById(int id)
+        {
+            var m = db.Movies.Find(id);
+            @ViewBag.movieId = id;
+            @ViewBag.movieTitle = m.Title;
+            return PartialView("_ActorsInMovie", m.Actors);
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.directors = db.Directors.ToList();
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie incomingMovie)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Movies.Add(incomingMovie);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public PartialViewResult CreateActor(Actor incomingActor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Actors.Add(incomingActor);
+                db.SaveChanges();
+                return ActorsById(incomingActor.selectedMovieId);
+            }
+            return null;
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            var mov = db.Movies.Find(id);
+            if (mov != null)
+            {
+                ViewBag.directors = db.Directors.ToList();
+                return View(mov);
+            }
+            else
+                return View();
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
